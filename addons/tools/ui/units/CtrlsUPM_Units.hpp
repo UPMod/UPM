5ad504a1b927a296cbbc6a398d95bd981a033a44
oscarmolinadev <oscarmolinadev@gmail.com>
2015-06-14 12:01:15 +0200

    #35: WIP UI Window

--- addons/tools/ui/units/CtrlsUPM_Units.hpp
+++ addons/tools/ui/units/CtrlsUPM_Units.hpp
@@ -4,8 +4,8 @@ class UnitsWindow: RscControlsGroup
 	text = "";
 	x = 5.44616e-005 * safezoneW + safezoneX + 0.05;
 	y = (0.1 * safezoneH + safezoneY) + (1.6 * 1) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25);
-	w = 0.8 * safezoneW;
-	h = 0.4048 * safezoneH;
+	w = 0.738 * safezoneW;
+	h = 0.8 * safezoneH;
 	class controls
 	{
 		class UnitsHeader: RscHeaderUPM_Window
@@ -13,17 +13,17 @@ class UnitsWindow: RscControlsGroup
 			idc = -1;
 			x = 0;
 			y = 0;
-			w = 0.8 * safezoneW;
+			w = 0.738 * safezoneW;
 			h = "1.4 * 			(			(			((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
 			text = "UNITS";
 		};
 		class UnitsBackground: RscBackgroundUPM_Window
 		{
 			idc = -1;
-			x = -0.00103095 * safezoneW;
+			x = 0;
 			y = 0.035 * safezoneH;
-			w = 0.8 * safezoneW;
-			h = 0.3803 * safezoneH;
+			w = 0.738 * safezoneW;
+			h = 0.7 * safezoneH;
 		};
 	};
 };
